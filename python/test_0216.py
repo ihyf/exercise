fb1f73e38d81b61233cce65965c9ac169524e9fd
黄应飞 <ihyf@huangyingfeideMacBook-Pro.local>
2017-02-16 17:39:04 +0800

    练习 对对象的属性排序

---
+++ python/test_0216.py
@@ -0,0 +1,20 @@
+# coding:utf-8
+import operator
+
+
+class Student(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+s1 = Student('Luna', 10)
+s2 = Student('Spe', 13)
+s3 = Student('Lina', 16)
+
+list_student = [s1, s2, s3]
+cmp_ful = operator.attrgetter('age')  # 以此属性排序
+list_student.sort(key=cmp_ful)
+list_student = list_student[::-1]
+for obj in list_student:
+    print(obj.name, obj.age)
+
